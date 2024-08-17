c7819870c465cf83db00c03ee40877f63750d72d
Adi <salmoneatenbybear@gmail.com>
2024-08-17 18:21:20 +0530

    updated for 16 Aug, removed janky things :(

--- script.js
+++ script.js
@@ -43,57 +43,6 @@ function toggleCalendar() {
     calendarContainer.style.display = (calendarContainer.style.display === "none" || calendarContainer.style.display === "") ? "block" : "none";
 }
 
-particlesJS("particles-js", {
-    "particles": {
-        "number": {
-            "value": 8,  
-            "density": {
-                "enable": true,
-                "value_area": 800
-            }
-        },
-        "shape": {
-            "type": "image",  
-            "image": {
-                "src": "monkey.png",  
-                "width": 1,  
-                "height": 1  
-            }
-        },
-        "opacity": {
-            "value": 0.5,
-            "random": true
-        },
-        "size": {
-            "value": 10,  
-            "random": true
-        },
-        "move": {
-            "enable": true,
-            "speed": 4,  
-            "direction": "none",
-            "random": true,
-            "straight": false,
-            "out_mode": "out",
-            "bounce": false
-        }
-    },
-    "interactivity": {
-        "detect_on": "canvas",
-        "events": {
-            "onhover": {
-                "enable": true,
-                "mode": "repulse"
-            },
-            "onclick": {
-                "enable": true,
-                "mode": "push"
-            },
-            "resize": true
-        }
-    },
-    "retina_detect": true
-});
 
 // Function to toggle light and dark mode
 function toggleTheme() {
